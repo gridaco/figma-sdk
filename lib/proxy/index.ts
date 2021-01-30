9f30e5911408addc64efbeb1dcbd4fb39ef163f3
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-31 01:10:18 +0900

    wip - proxy handler

--- lib/proxy/index.ts
+++ lib/proxy/index.ts
@@ -1,10 +1,21 @@
+import { TargetPlatform } from "../platform";
+
 abstract class ProxyClient {
-    abstract apiVersion: string
+  readonly platform: TargetPlatform;
+  constructor(props: { platform: TargetPlatform }) {
+    // figma.getNodeById();
+  }
+  abstract apiVersion: string;
+
+  async fetchNode(id: string) {
+    if (this.platform == TargetPlatform.figma) {
+      return figma.getNodeById(id);
+    }
+  }
 }
 
-
 class FigmaProxyClient extends ProxyClient {
-    get apiVersion() {
-        return figma.apiVersion
-    }
+  get apiVersion() {
+    return figma.apiVersion;
+  }
 }
