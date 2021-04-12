9454d23356c3b74c2de6db959f9bee6f21ec0599
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-13 05:19:15 +0900

    isolate module fix - export directly

--- lib/nodes/types/index.ts
+++ lib/nodes/types/index.ts
@@ -1,35 +1,18 @@
-import { IReflectNodeReference, ReflectBaseNode } from "./base.node";
-import { ReflectEllipseNode } from "./ellipse.node";
-import { ReflectFrameNode } from "./frame.node";
-import { ReflectGroupNode } from "./group.node";
-import { ReflectLineNode } from "./line.node";
-import { ReflectBlendMixin, ReflectChildrenMixin, ReflectConstraintMixin, ReflectCornerMixin, ReflectDefaultShapeMixin, ReflectGeometryMixin, ReflectLayoutMixin } from "./mixins";
-import { ReflectSceneNode, ReflectSceneNodeType } from "./node-type";
-import { ReflectRectangleNode } from "./rectangle.node";
-import { ReflectTextNode, TextAlign, TextAlignVertical } from "./text.node";
-import { mixed } from "./mixed"
-
-
+export { IReflectNodeReference, ReflectBaseNode } from "./base.node";
+export { ReflectEllipseNode } from "./ellipse.node";
+export { ReflectFrameNode } from "./frame.node";
+export { ReflectGroupNode } from "./group.node";
+export { ReflectLineNode } from "./line.node";
 export {
-    mixed,
-    ReflectSceneNode,
-    ReflectSceneNodeType,
-    ReflectRectangleNode,
-    ReflectEllipseNode,
-    ReflectBaseNode,
-    ReflectFrameNode,
-    ReflectGroupNode,
-    TextAlign,
-    TextAlignVertical,
-    ReflectTextNode,
-    ReflectLineNode,
-    ReflectLayoutMixin,
-    ReflectConstraintMixin,
-    ReflectChildrenMixin,
-    ReflectDefaultShapeMixin,
-    ReflectBlendMixin,
-    ReflectGeometryMixin,
-    ReflectCornerMixin,
-    IReflectNodeReference
-}
-
+  ReflectBlendMixin,
+  ReflectChildrenMixin,
+  ReflectConstraintMixin,
+  ReflectCornerMixin,
+  ReflectDefaultShapeMixin,
+  ReflectGeometryMixin,
+  ReflectLayoutMixin,
+} from "./mixins";
+export { ReflectSceneNode, ReflectSceneNodeType } from "./node-type";
+export { ReflectRectangleNode } from "./rectangle.node";
+export { ReflectTextNode, TextAlign, TextAlignVertical } from "./text.node";
+export { mixed } from "./mixed";
