7a7724cc42d7be632bd1bb1830b4af09b86b4c15
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-13 18:32:03 +0900

    fix sketch text align conversion - support undefined case

--- lib/sketch/converters/text-align.convert.ts
+++ lib/sketch/converters/text-align.convert.ts
@@ -3,6 +3,10 @@ import { TextAlign } from "@reflect-ui/core";
 export function convertSketchTextAligntmentToReflectTextAlignment(
   sketch: TextHorizontalAlignment
 ): TextAlign {
+  if (sketch === undefined) {
+    return TextAlign.start;
+  }
+
   switch (sketch) {
     case TextHorizontalAlignment.Centered:
       return TextAlign.center;
@@ -15,5 +19,6 @@ export function convertSketchTextAligntmentToReflectTextAlignment(
     case TextHorizontalAlignment.Right:
       return TextAlign.right;
   }
+
   throw `${sketch} is not supported for reflect conversion`;
 }
