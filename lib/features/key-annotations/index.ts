8f06e637e1f7c04d0f79a404926590979525de85
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-24 17:00:56 +0900

    update special key util

--- lib/features/key-annotations/index.ts
+++ lib/features/key-annotations/index.ts
@@ -1,6 +1,21 @@
 import { SpecialKeys, SPECIAL_KEY_PREFIX_TOKEN } from "./keys";
-import { SpecialKeyResult, IgnoranceType } from "./types";
+import { SpecialKeyResult, SpecialKeyType } from "./types";
 export * from "./keys";
+
+/**
+ * case 1. //@ignore/some-name : null
+ * case 2. //@data-target/some-name : "some-name"
+ * case 3. Rectangle 1 : "Rectangle 1"
+ * @param name
+ * @returns
+ */
+export function normalizedName(name: string) {
+  if (specialKeyTypeFrom(name) == SpecialKeys.KEY_IGNORE_ALL) {
+    return null;
+  }
+
+  return name.replace(SPECIAL_KEY_PREFIX_TOKEN, "");
+}
 
 export function shouldIgnore(name: string): boolean {
   const result = specialKeyAnalysis(name);
@@ -25,7 +40,7 @@ export function specialKeyAnalysis(name: string): SpecialKeyResult {
   };
 }
 
-export function specialKeyTypeFrom(name: string): IgnoranceType {
+export function specialKeyTypeFrom(name: string): SpecialKeyType {
   if (name.includes(SPECIAL_KEY_PREFIX_TOKEN)) {
     // TODO support glob pattern matching
     for (const kk of Object.keys(SpecialKeys)) {
