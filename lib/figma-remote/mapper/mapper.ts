450f31e65e0d172aea70ba89b71cd2e421214fca
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-01 05:35:56 +0900

    add vanilla & assets repository from design-to-codes since it design sdk is more adequate place to be.

--- lib/figma-remote/mapper/mapper.ts
+++ lib/figma-remote/mapper/mapper.ts
@@ -2,6 +2,14 @@ import { SceneNode } from "../../figma/types/v1";
 import { Node } from "../types";
 import { mapFigmaRemoteTextToFigma } from "./text.mapper";
 export function mapFigmaRemoteToFigma(remNode: Node): SceneNode {
+  if ("children" in remNode) {
+    const mappedChildren = remNode.children.map((c) =>
+      mapFigmaRemoteToFigma(c)
+    );
+
+    // @ts-ignore
+    remNode = { ...remNode, children: mappedChildren };
+  }
   switch (remNode.type) {
     case "TEXT":
       return mapFigmaRemoteTextToFigma(remNode);
