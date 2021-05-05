dcbda037d20a7f7b94089d94d36809f18ab2bce3
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-05 22:39:23 +0900

    add extra node types for raw design node support

--- core/nodes/types/node-type.ts
+++ core/nodes/types/node-type.ts
@@ -18,3 +18,27 @@ export enum ReflectSceneNodeType {
   image = "IMAGE",
   unknown = "UNKNOWN",
 }
+
+/**
+ * ReflectSceneNodeType + Primitives e.g. star
+ */
+export enum ReflectNodeType {
+  group = "GROUP",
+  component = "COMPONENT", // this should be not a type, but a property
+  // TODO: consider renaming to variant-set. the name variant is ambigious
+  variant_set = "COMPONENT_SET", // this should be not a type, but a property
+  constraint = "CONSTRAINT",
+  instance = "INSTANCE", // this should be not a type, but a property
+  text = "TEXT",
+  frame = "FRAME",
+  ellipse = "ELLIPSE",
+  rectangle = "RECTANGLE",
+  line = "LINE",
+  vector = "VECTOR",
+  image = "IMAGE",
+  unknown = "UNKNOWN",
+  ////////////////////////
+  ////////////////////////
+  star = "STAR",
+  poligon = "POLIGON",
+}
