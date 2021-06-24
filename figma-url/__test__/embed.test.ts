cfa2be859e3a92ec0723a27486980dab68247987
UZU, J <woojoo@softmarshmallow.com>
2021-06-25 03:37:18 +0900

    add extra test case

--- figma-url/__test__/embed.test.ts
+++ figma-url/__test__/embed.test.ts
@@ -1,8 +1,15 @@
-import { __FIGMA_DEMO_DEFAULT_FILE_NODE_URL } from "../lib";
 import { embed } from "../lib";
 
-test("parse fileid from url", () => {
-  expect(embed(__FIGMA_DEMO_DEFAULT_FILE_NODE_URL)).toBe(
+test("build embed url", () => {
+  const _url =
+    "https://www.figma.com/file/Y0Gh77AqBoHH7dG1GtK3xF/?node-id=264%3A49";
+  expect(embed(_url)).toBe(
+    "https://www.figma.com/embed?embed_host=astra&url=https://www.figma.com/file/Y0Gh77AqBoHH7dG1GtK3xF/?node-id=264%3A49"
+  );
+});
+
+test("build embed url with file & node id", () => {
+  expect(embed({ fileid: "Y0Gh77AqBoHH7dG1GtK3xF", nodeid: "264%3A49" })).toBe(
     "https://www.figma.com/embed?embed_host=astra&url=https://www.figma.com/file/Y0Gh77AqBoHH7dG1GtK3xF/?node-id=264%3A49"
   );
 });
