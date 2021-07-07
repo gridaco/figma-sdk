51bba9439bd1084ed9f97ce71ea68dc192810fcb
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-07 18:27:30 +0900

    add design url analysis module

---
+++ url-analysis/index.ts
@@ -0,0 +1,45 @@
+export type DesignProvider =
+  | "grida"
+  | "nothing"
+  | "figma"
+  | "sketch"
+  | "unknown";
+
+export function analyzeDesignUrl(url: string): DesignProvider {
+  try {
+    const u = new URL(url);
+    switch (u.hostname) {
+      /**
+       * https://figma.com/file/~
+       */
+      case "figma.com":
+      case "www.figma.com":
+        return "figma";
+        break;
+      /**
+       * https://sketch.com/s/~
+       */
+      case "www.sketch.com":
+      case "sketch.com":
+        return "sketch";
+        break;
+
+      /**
+       * powered by nothing graphics engine
+       */
+      case "www.nothing.app":
+      case "nothing.app":
+        return "nothing";
+      case "www.bridged.xyz":
+      case "www.grida.co":
+      case "grida.co":
+      case "bridged.xyz": // legacy bridged.xyz urls
+        return "grida";
+      default:
+        return "unknown";
+    }
+  } catch (_) {
+    // console.warn(`failed analyzing url. the url "${url}" is not a valid url.`)
+    return "unknown";
+  }
+}
