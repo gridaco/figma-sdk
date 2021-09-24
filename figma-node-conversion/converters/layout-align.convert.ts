cb8b77586deb067d2c77a70259e0e957b91c6641
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-25 08:11:37 +0900

    add undef support

--- figma-node-conversion/converters/layout-align.convert.ts
+++ figma-node-conversion/converters/layout-align.convert.ts
@@ -21,6 +21,8 @@ export function layoutAlignToReflectMainAxisSize(
        * - If the current node is an auto layout frame (e.g. an auto layout frame inside a parent auto layout frame) if you set layoutAlign to “STRETCH” you should set the corresponding axis – either primaryAxisSizingMode or counterAxisSizingMode – to be“FIXED”. This is because an auto-layout frame cannot simultaneously stretch to fill its parent and shrink to hug its children.
        */
       return MainAxisSize.max;
+    case undefined:
+      return;
   }
 
   // error region
