ef8620e455a6ea43326877003fd9cdd7e1378c1a
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-26 18:38:20 +0900

    wip

--- figma-oauth-react/lib/use-figma-auth-state/index.ts
+++ figma-oauth-react/lib/use-figma-auth-state/index.ts
@@ -1,3 +1,8 @@
+import React from "react";
+import "@design-sdk/figma-oauth";
+import { getAccessToken_safe } from "@design-sdk/figma-auth-store";
 export function useFigmaAuthState() {
+  const accessToken_safe = getAccessToken_safe();
+
   //
 }
