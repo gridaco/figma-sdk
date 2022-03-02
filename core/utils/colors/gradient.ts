7cd40b31281ee284b797dd0911a16051c95a7e39
softmarshmallow <woojoo@softmarshmallow.com>
2022-03-03 01:50:00 +0900

    add docs

--- core/utils/colors/gradient.ts
+++ core/utils/colors/gradient.ts
@@ -1,4 +1,4 @@
-import { GradientPaint } from "@design-sdk/figma";
+import type { GradientPaint } from "@design-sdk/figma";
 
 export function gradientAngle(fill: GradientPaint): number {
   const decomposed = decomposeRelativeTransform(
@@ -9,7 +9,12 @@ export function gradientAngle(fill: GradientPaint): number {
   return (decomposed.rotation * 180) / Math.PI;
 }
 
-// from https://math.stackexchange.com/a/2888105
+/**
+ * from https://math.stackexchange.com/a/2888105
+ * @param t1 from 3x3 matrix[0]
+ * @param t2 from 3x3 matrix[1]
+ * @returns
+ */
 export function decomposeRelativeTransform(
   t1: [number, number, number],
   t2: [number, number, number]
@@ -41,21 +46,19 @@ export function decomposeRelativeTransform(
   };
 
   // Apply the QR-like decomposition.
-  if (a !== 0 || b !== 0) {
-    const r = Math.sqrt(a * a + b * b);
+  if (a != 0 || b != 0) {
+    var r = Math.sqrt(a * a + b * b);
     result.rotation = b > 0 ? Math.acos(a / r) : -Math.acos(a / r);
     result.scale = [r, delta / r];
     result.skew = [Math.atan((a * c + b * d) / (r * r)), 0];
+  } else if (c != 0 || d != 0) {
+    var s = Math.sqrt(c * c + d * d);
+    result.rotation =
+      Math.PI / 2 - (d > 0 ? Math.acos(-c / s) : -Math.acos(c / s));
+    result.scale = [delta / s, s];
+    result.skew = [0, Math.atan((a * c + b * d) / (s * s))];
+  } else {
+    // a = b = c = d = 0
   }
-  // these are not currently being used.
-  // else if (c != 0 || d != 0) {
-  //   const s = Math.sqrt(c * c + d * d);
-  //   result.rotation =
-  //     Math.PI / 2 - (d > 0 ? Math.acos(-c / s) : -Math.acos(c / s));
-  //   result.scale = [delta / s, s];
-  //   result.skew = [0, Math.atan((a * c + b * d) / (s * s))];
-  // } else {
-  //   // a = b = c = d = 0
-  // }
   return result;
 }
