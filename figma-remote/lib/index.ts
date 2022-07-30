499b97a1ea00315e4cb0329d2c7c389c092bf005
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-30 19:53:25 +0900

    update package configs

--- figma-remote/lib/index.ts
+++ figma-remote/lib/index.ts
@@ -1,6 +1,7 @@
 export * as types from "@design-sdk/figma-remote-types";
 export * as mapper from "./mapper";
 export { configure_auth_credentials } from "./configure-auth-credentials";
-
 /* this will be removed ->> */ export * as fetch from "./fetch";
 /* preserve this only ->> */ export * from "./fetch";
+
+export * from "./asset-repository";
