dd836272886b61d4786c08f3348d5ba2da667928
Universe <universe@grida.co>
2023-12-26 21:39:12 +0900

    add token expired error

--- figma-remote/lib/fetch/errors.ts
+++ figma-remote/lib/fetch/errors.ts
@@ -9,6 +9,26 @@ export class UnauthorizedError extends Error {
   }
 }
 
+/**
+ * This error is thrown when the token is expired.
+ *
+ * Example response:
+ * ```
+ * {
+ *  "status": 403,
+ *  "err": "Token expired"
+ * }
+ * ```
+ */
+export class TokenExpiredUnauthorizedError extends Error {
+  readonly status = 403;
+  readonly type = "403_token_expired";
+  constructor(message: string) {
+    super(message);
+    this.name = "403_token_expired";
+  }
+}
+
 export class NotfoundError extends Error {
   readonly status = 404;
   readonly type = "NotFound";
